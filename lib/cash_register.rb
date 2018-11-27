cb382e474aad18670a8dae550acce2362f2d6891
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-27 01:03:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+require "pry"
+class CashRegister
+  
+  attr_accessor :total, :quantity 
+  
+  def initialize(total = 0)
+    @total = total
+    @quantity
+  
+    def cash_register_with_discount
+      self.apply_discount
+      
+    end
+  end
+
+  
+  def add_item(title, price, quantity = 1)
+    @total = @total + (price*quantity)
+ 
+  # binding.pry
+  end 
+  
+  def apply_discount
+    #context 'the cash register was initialized with an employee discount' do
+      #it 'applies the discount to the total price' do
+        #cash_register_with_discount.add_item("macbook air", 1000)
+        #cash_register_with_discount.apply_discount
+        #expect(cash_register_with_discount.total).to eq(800)  
+    x = add_item(price)
+      
+    binding.pry
+  end 
+  
+  def items
+    
+  end 
+  
+  def void_last_transaction
+    
+  end 
+end
+
 
